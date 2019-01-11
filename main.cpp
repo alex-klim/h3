3b8f0969a903e6b786842758712879d73e8508c5
Oleksandr Klymenko <olek.klymenko@gmail.com>
2019-01-11 15:45:23 +0200

    add better test representation

--- main.cpp
+++ main.cpp
@@ -3,16 +3,18 @@
 int main() {
     Date dt;
     std::cin >> dt;
+    std::cout << "dt: " << dt << '\n';
     Date dt2("1212315.04.2542asdasd");
     std::cout << "dt2: " << dt2 << '\n';
-    std::cout << "changing format of dt1 to mmddyyyy\n";
+    std::cout << "changing format of dt to mmddyyyy:\n";
     dt.setFormat(Format::mmddyyyy);
+    std::cout << dt << '\n';
     std::cout << "**********************************************\n";
     int temp = dt2 - dt;
-    std::cout << "dt2 - dt1 = " << temp << '\n';
+    std::cout << "dt2 - dt = " << temp << '\n';
     std::cout << "**********************************************\n";
     dt.applyDelta(temp);
-    std::cout << "dt.applyDelta:\n" << dt << '\n';
+    std::cout << "dt.applyDelta(" << temp << "):\n" << dt << '\n';
 
     return 0;
 }
